1e285714da52eb4f413c3657409d1776f3b4c002
madvin <madvin@abv.bg>
2025-11-02 11:54:04 +0200

    reworked index.html and add sample todos

--- reactjs-toDoList/src/components/TodoCard.jsx
+++ reactjs-toDoList/src/components/TodoCard.jsx
@@ -1,5 +1,12 @@
-export default function TodoCard() {
-  return (
-    <>TodoCard</>
-  );
+export default function TodoCard(props) {
+    const { children } = props;
+    return (
+        <li className='todoItem' >
+            {children}
+            <div className='actionsContainer'>
+                 <i className="fa-solid fa-pen-to-square"></i>
+                 <i className="fa-regular fa-trash-can"></i>
+            </div>  
+        </li>
+    );
 }
--- reactjs-toDoList/src/components/TodoList.jsx
+++ reactjs-toDoList/src/components/TodoList.jsx
@@ -1,5 +1,22 @@
+import TodoCard from "./TodoCard"
+
 export default function TodoList() {
-  return (
-    <>TodoList</>
-  )
+
+    let todos = [
+        'Sample Todo 1',
+        'Sample Todo 2',
+        'Sample Todo 3'
+    ]
+
+    return (
+        <ul className='main'>
+            {todos.map((todo, todoIndex) => {
+                return (
+                   <TodoCard key ={todoIndex}>
+                    <p>{todo}</p>
+                   </TodoCard>
+                )
+            })}
+        </ul>
+    )
 }
