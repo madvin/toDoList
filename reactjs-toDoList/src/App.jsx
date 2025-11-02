abd0fdf911cfd6ccb2e416807934f91dd9bd7227
madvin <madvin@abv.bg>
2025-11-02 14:49:47 +0200

    add persisted data to local storage

--- reactjs-toDoList/src/App.jsx
+++ reactjs-toDoList/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect, use } from "react"
 
 import TodoList from "./components/TodoList"
 import TodoInput from "./components/TodoInput"
@@ -8,8 +8,16 @@ function App() {
 	const [todos, setTodos] = useState([]);
 	const [todoValue, setTodoValue] = useState('');
 
+	function persistData(newList) {
+		localStorage.setItem('todos', JSON.stringify({
+			todos:
+				newList
+		}))
+	}
+
 	function handleAddTodos(newTodo) {
 		const newTodoList = [...todos, newTodo]
+		persistData(newTodoList)
 		setTodos(newTodoList)
 	}
 
@@ -17,6 +25,7 @@ function App() {
 		const newTodoList = todos.filter((todo, index) => {
 			return index !== todoIndex
 		})
+		persistData(newTodoList)
 		setTodos(newTodoList)
 	}
 
@@ -26,17 +35,25 @@ function App() {
 		handleDeleteTodo(index)
 	}
 
+	useEffect(() => {
+		if (!localStorage) return
+		let localTodos = localStorage.getItem('todos')
+		if (!localTodos) return
+		localTodos = JSON.parse(localTodos)
+		setTodos(localTodos)
+	}, [])
+
 
 	return (
 		<>
-			<TodoInput 
-			todoValue={todoValue} 
-			setTodoValue={setTodoValue}
-			handleAddTodos={handleAddTodos}/>
-			<TodoList 
-			handleEditTodo={handleEditTodo}
-			handleDeleteTodo={handleDeleteTodo}
-			 todos={todos}/>
+			<TodoInput
+				todoValue={todoValue}
+				setTodoValue={setTodoValue}
+				handleAddTodos={handleAddTodos} />
+			<TodoList
+				handleEditTodo={handleEditTodo}
+				handleDeleteTodo={handleDeleteTodo}
+				todos={todos} />
 		</>
 	)
 }
