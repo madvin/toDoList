65156ec291a6e38b93aeeeb2edc41842398f37a3
madvin <madvin@abv.bg>
2025-11-02 14:50:21 +0200

    fix import in App.jsx

--- reactjs-toDoList/src/App.jsx
+++ reactjs-toDoList/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, use } from "react"
+import { useState, useEffect } from "react"
 
 import TodoList from "./components/TodoList"
 import TodoInput from "./components/TodoInput"
